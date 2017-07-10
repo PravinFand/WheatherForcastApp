b16d715312460ad5abacf44efbd36606df3cc201
Fand, Pravin : Group Centre <Pravin.Fand@barclayscorp.com>
2017-07-10 19:24:37 +0530

    Get Wheather from API added.

--- WheatherForcast/WeatherForcaastDetailsViewController.m
+++ WheatherForcast/WeatherForcaastDetailsViewController.m
@@ -7,32 +7,62 @@
 //
 
 #import "WeatherForcaastDetailsViewController.h"
+#import "KFOpenWeatherMapAPIClient.h"
+
+#import "KFOWMWeatherResponseModel.h"
+#import "KFOWMMainWeatherModel.h"
+#import "KFOWMWeatherModel.h"
+#import "KFOWMForecastResponseModel.h"
+#import "KFOWMCityModel.h"
+#import "KFOWMDailyForecastResponseModel.h"
+#import "KFOWMDailyForecastListModel.h"
+#import "KFOWMSearchResponseModel.h"
+#import "KFOWMSystemModel.h"
+
 
 @interface WeatherForcaastDetailsViewController ()
+    
+@property (nonatomic, strong) KFOpenWeatherMapAPIClient *apiClient;
+@property (nonatomic, strong) KFOWMWeatherResponseModel *responseModel;
 
 @end
 
 @implementation WeatherForcaastDetailsViewController
-
+    
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    [self.view setBackgroundColor:[UIColor yellowColor]];
+    
+    self.apiClient = [[KFOpenWeatherMapAPIClient alloc] initWithAPIKey:@"c6e381d8c7ff98f0fee43775817cf6ad" andAPIVersion:@"2.5"];
+    
+    [self.apiClient weatherForCityName:@"Pune" withResultBlock:^(BOOL success, id responseData, NSError *error)
+     {
+         if (success)
+         {
+            _responseModel = (KFOWMWeatherResponseModel *)responseData;
+             NSLog(@"received weather: %@, temperature: %@ K, %@%%rH, %@ mbar", _responseModel.cityName, _responseModel.mainWeather.temperature, _responseModel.mainWeather.humidity, _responseModel.mainWeather.pressure);
+         }
+         else
+         {
+             NSLog(@"could not get weather: %@", error);
+         }
+     }];
 }
-
+    
 - (void)didReceiveMemoryWarning {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-@end
+    
+    /*
+     #pragma mark - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @end
